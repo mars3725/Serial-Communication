43bd692fe3c4fa3476ac2db16b27632386976385
mars3725 <mattmohandiss@gmail.com>
2016-04-21 11:17:05 -0400

    Initial Commit

---
+++ SerialCommunication/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SerialCommunication
+//
+//  Created by Matthew Mohandiss on 4/21/16.
+//  Copyright © 2016 Matthew Mohandiss. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SerialCommunication/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SerialCommunication
+//
+//  Created by Matthew Mohandiss on 4/21/16.
+//  Copyright © 2016 Matthew Mohandiss. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
